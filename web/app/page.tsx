bbcc9024d43de91ee3ed3cb50bf42aa34f3369b1
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-07-28 10:37:03 +0530

    feat:added next-auth and google oauth

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
+import {Button} from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Todist
+     <h1>Todist</h1>
+     <Button variant = {""}>Hello </Button>
+
+
+    {/* <form action ={signInAction}>
+        <Button> Log in </Button>  
+    </form> */}
     </main>
   );
 }
---
+++ web/lib/auth.ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialProvider from "next-auth/providers/credentials";
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+
+    //credentials
+
+    CredentialProvider({
+        name:"Email",
+        credentials:{
+            emial:{label:"Email" ,type:"email" ,placeholder:"Enter your email.."},
+            password:{label:"Password" ,type:"password" ,placeholder:"Enter your password.."}
+        },
+        async authorize(credentials :any){
+            console.log(credentials)
+            //validation
+            return {
+                id:"user1",
+                name:"abc",
+                email:"abc@example.com"
+            }
+        }
+    }),
+
+    // google o auth
+    GoogleProvider({
+        clientId:process.env.GOOGLE_CLIENT_ID,
+        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        authorization:{
+            params:{prompt :"consent"},
+        }
+    }),
+
+    //github o auth
+  ],
+})
